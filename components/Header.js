13921ac0078b437f8df9cd47b4eb16fc6c0b99fd
peter vogt <peter@petervogt.co>
2022-07-25 19:10:27 -0500

    main header style cleanup

--- components/Header.js
+++ components/Header.js
@@ -8,19 +8,21 @@ const Header = () => {
   return (
     <header className="px-8 pt-8 pb-4 text-slate-900 dark:text-eggshell-50">
       <div className="flex w-full items-center border-b border-slate-700/50 pb-4 dark:border-slate-100/50 md:justify-between lg:border-b-0 lg:pb-0">
-        <ul className="flex w-full flex-col items-start sm:w-full md:w-full md:flex-row md:items-center lg:w-2/3">
+        <ul className="flex w-full flex-col items-start whitespace-pre text-center sm:w-full md:w-full md:flex-row md:items-center lg:w-6/12">
           <li className="pr-0 lg:pr-8">
             <h1 className="font-serif text-7 font-semibold tracking-1 md:text-9 md:leading-8 lg:text-11 lg:leading-10 xl:text-13">
               Peter Vogt
             </h1>
           </li>
-          <li className="hidden w-full flex-1 flex-shrink border-l border-r border-slate-700/50 py-4 pl-8 pr-8 align-middle dark:border-slate-100/50 xl:block">
-            <h2 className="text-left font-sans text-base font-normal leading-normal">
-              Product Designer, Design Technologist
+          <li className="hidden w-full flex-1 flex-shrink border-l border-r border-slate-700/50 py-4 pl-8 pr-8 dark:border-slate-100/50 xl:block">
+            <h2 className="  text-center font-sans text-base font-normal leading-normal">
+              Product Designer / Design Technologist
             </h2>
           </li>
-          <li className="hidden w-full flex-1 border-r border-slate-700/50 py-6 pl-8 pr-8 align-middle dark:border-slate-100/50 xl:block">
-            <h2 className="text-left font-sans text-base font-normal leading-normal">Currently Sr. Designer @ USAA</h2>
+          <li className="hidden w-full flex-1 border-r border-slate-700/50 py-6 pl-8 pr-8 dark:border-slate-100/50 xl:block">
+            <h2 className="text-center font-sans text-base font-normal leading-normal">
+              Currently Sr. Designer @ USAA
+            </h2>
           </li>
         </ul>
         <div className="flex items-center">
@@ -32,7 +34,9 @@ const Header = () => {
         </div>
       </div>
 
-      <h2 className="block py-6 lg:hidden">Product Designer & Design Technologist. Currently: Sr. Designer @ USAA</h2>
+      <h2 className="block whitespace-pre-line py-6 lg:hidden">
+        Product Designer & Design Technologist. <strong>Currently:</strong> Sr. Designer @ USAA
+      </h2>
       <Nav />
     </header>
   )
